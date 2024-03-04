a323ee488728e41fddcc6cd66f02d9ba037b9baf
JULIO08050 <161668067+JULIO08050@users.noreply.github.com>
2024-03-04 14:47:59 -0800

    proyecto.php

---
+++ proyecto.php
@@ -0,0 +1,45 @@
+<?php
+//AGREGAMOS NUESTROS OBJETOS EN ESTE CASO SON CARROS
+//primer carro
+$carro1=[
+  "marca"=>"toyota",
+  "modelo"=>"",
+"color_de_pintura"=>"",
+
+
+  
+  
+
+  
+
+
+
+
+
+
+
+
+
+
+
+];  
+//SEGUNDO CARRO
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
