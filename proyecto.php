ab18ca52741e4d2299beb6bdf5933ee7a5bce566
misael1223 <161668313+misael1223@users.noreply.github.com>
2024-03-04 15:09:55 -0800

    Update proyecto.php

--- proyecto.php
+++ proyecto.php
@@ -24,20 +24,32 @@ $carro1=[
 
 ];  
 //SEGUNDO CARRO
+$carro2=[
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+"marca"=>"jeep",
+"modelo"=>"WRANGLER_Rubicon_2024",
+"color_de_pintura"=>"negro_mate",
+"Potencia"=>"285/6400 hp/rpm",
+"Cilindros"=>"6_en_V",
+"Válvulas"=>"24",
+"Rendimiento_en_ciudad"=>"6.6km/l",
+"Rendimiento_en_ruta"=>"10.4km/l",
+"Rendimiento_mixto"=>"7.9km/l",
+"Motor-Tracción"=>"delantero-integral",
+"Transmisión"=>"automática_8_velocidades",
+"Neumáticos"=>"255/75/R17",
+"Suspensión_delantera"=>Eje_rígido_con_resortes_helicoidales,brazos_paralelos,barra_estabilizadora_y_amortiguadores_de_trabajo_pesado",
+"Dirección_asistida"=>"electro-hidráulica",
+"Largo"=>"4237mm", 
+"Ancho_sin_espejos"=>"1875mm",
+"Alto"=>"1868mm",
+"Distancia_entre_ejes"=>"2460mm",
+"Peso"=>"1794kg",
+"Tanque_de_combustible"=>"66L",  
+"Capacidad_de_carga"=>"454kg",
+];
+//carro3 
+  
 
 
 
